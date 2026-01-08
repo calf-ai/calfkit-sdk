07d1a72cb4e94bd833d4c752c5269222dc579a92
Ryan <ryany14@icloud.com>
2026-01-08 15:16:49 -0700

    fix: add __version__ to calf module

--- calf/__init__.py
+++ calf/__init__.py
@@ -1,4 +1,7 @@
+from importlib.metadata import version
+
 from calf.client import Calf
 from calf.message import Message
 
-__all__ = ["Calf", "Message"]
+__version__ = version("calf-sdk")
+__all__ = ["Calf", "Message", "__version__"]
